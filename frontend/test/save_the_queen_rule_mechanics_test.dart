feb6a48c628fe5480c184546bafee3d1ee10dbe1
metaphy6 <serhatakbak@outlook.com>
2026-04-09 10:52:38 +0300

    chore: flutter doctor fixes

--- frontend/test/save_the_queen_rule_mechanics_test.dart
+++ frontend/test/save_the_queen_rule_mechanics_test.dart
@@ -1,5 +1,4 @@
 import 'package:chessrecast/board/utils/exporter.dart';
-import 'package:chessrecast/board/moves/position.dart';
 import 'package:chessrecast/management/orchestrator.dart';
 import 'package:chessrecast/mods/enums.dart';
 import 'package:flutter_test/flutter_test.dart';
